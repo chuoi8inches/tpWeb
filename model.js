d0f3ac55459dcdc97c7d32a7cdd89fd481e9130a
chuoi8inches <anhnguyenhuu9a1@gmail.com>
2023-09-27 22:34:33 +0200

    Q6

--- model.js
+++ model.js
@@ -1,3 +1,29 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function Drawing(){
+    var shapeTab = Array()
+    this.addShape =function(color, thickness){
+    }
+}
+function Shape(color, thickness){
+    this.color = color
+    this.thickness = thickness
+}
+function Rectangle(upleftCornerX, upleftCornerY, width, height, thickness, color) {
+    Shape.call(this,thickness,color)
+    this.upleftCornerX = upleftCornerX
+    this.upleftCornerY = upleftCornerY
+    this.width = width
+    this.height = height
+    this.paint = function(context){
+    }
+}
+function Line(startX,startY, endX, endY) {
+    Shape.call(this, color, thickness)
+    this.startX = startX
+    this.startY =startY
+    this.endX = endX
+    this.endY =endY
+
+}
